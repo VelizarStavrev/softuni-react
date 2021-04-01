eb817c6158a2a1216f540d6ae405f630662e7c3a
VelizarStavrev <velizar.g.stavrev@gmail.com>
2021-04-01 16:13:46 +0300

    Server updates - fixing CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-const serverHeaders = require('./middlewares/serverHeaders');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const { PORT } = require('./config/config');
@@ -9,7 +8,13 @@ const app = express();
 
 require('./config/mongoose');
 
-app.use(serverHeaders);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
 app.use(routes);
 
